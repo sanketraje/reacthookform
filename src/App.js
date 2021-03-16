f50919d62deca88eee94352a80844f4cfd8f12a0
sanketraje <rajesanket1998@gmail.com>
2021-03-16 21:49:07 +0530

    project addes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import {useForm} from 'react-hook-form';
 
 function App() {
+  const {register, handleSubmit, errors} = useForm();
+
+  const onSubmit=(data)=>{
+   console.log(data)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <center>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input type="text" placeholder="Email" name="email" ref={register} /><br></br><br/>
+      <input type="password" placeholder="Password" name="pass" ref={register({required:true})} /><br></br><br/>
+      {errors.pass && <p>Password is invalid</p>}
+      <input type="submit" />
+    </form>
+    </center>
+
   );
 }
 
